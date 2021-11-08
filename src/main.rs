033d024b2dc70115e01bb3889184099a5cae0723
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-08 18:22:41 +0545

    Added divide func

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,16 @@ fn muntiply(x: i32, y: i32) -> i32 {
     return x * y;
 }
 
+fn divide(x: i32, y: i32) -> i32 {
+    return x / y;
+}
+
 fn main() {
     let num1 = 20;
     let num2 = 22;
 
     println!("{} + {} = {}", num1, num2, add(num1, num2));
     println!("{} - {} = {}", num1, num2, subtract(num1, num2));
-    println!("{} - {} = {}", num1, num2, muntiply(num1, num2));
+    println!("{} * {} = {}", num1, num2, muntiply(num1, num2));
+    println!("{} / {} = {}", num1, num2, divide(num1, num2));
 }
