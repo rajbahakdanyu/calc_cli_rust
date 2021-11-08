ae7bdd781d21b5e47aa30d7147b6949e7fff793f
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-08 18:20:46 +0545

    Added subtract function

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ fn add(x: i32, y: i32) -> i32 {
     return x + y;
 }
 
+fn subtract(x: i32, y: i32) -> i32 {
+    return x - y;
+}
+
 fn main() {
     let num1 = 20;
     let num2 = 22;
 
     println!("{} + {} = {}", num1, num2, add(num1, num2));
+    println!("{} - {} = {}", num1, num2, subtract(num1, num2));
 }
