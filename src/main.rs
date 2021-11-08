ca74748ad80e79842b9dbe8b1c01d834db717ad0
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-08 16:39:32 +0545

    Added Add func

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+fn add(x: i32, y: i32) -> i32 {
+    return x + y;
+}
+
 fn main() {
     let num1 = 20;
     let num2 = 22;
 
-    let sum = &num1 + &num2;
-
-    println!("{}", sum);
+    println!("{} + {} = {}", num1, num2, add(num1, num2));
 }
